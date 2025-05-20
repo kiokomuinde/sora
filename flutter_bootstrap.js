b8e7ef81963198c5b2f165bb4b27491e5af84ae6
Kioko Muinde <kiokomuinde022@gmail.com>
2025-05-20 17:37:19 +0300

    Updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3163544021"
+    serviceWorkerVersion: "817810118"
   }
 });
