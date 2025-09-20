7eff0fa90e4406779c67b7ce099d6e2579542630
Choxx Delit <kiokomuinde022@gmail.com>
2025-09-20 18:56:59 +0300

    Fixed property details cards in view property

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3989574748"
+    serviceWorkerVersion: "943524280"
   }
 });
--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -224,14 +224,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "5c23393fac1002d722105619be7bb902
 "CNAME": "e1387523602c19a789f63aef496c64ed",
 "favicon.png": "b73dfb25a7a73361cca712dbd918df06",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "d51d74393739628a7e709061d0dd82ed",
+"flutter_bootstrap.js": "4ec877e126932ef16bc9e55c143e5f0e",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "aa13e9dc757e01054c55768e2ea3892b",
 "/": "aa13e9dc757e01054c55768e2ea3892b",
-"main.dart.js": "3e788a5b2e66774d8053cf789648a0a1",
+"main.dart.js": "86e91247c49d16b3768828dfd7645982",
 "manifest.json": "e2d8aacf924b8886c558ef3a42acc6ec",
 "version.json": "b75bad869b2040f1993e54d13f3df6c6"};
 // The application shell files that are downloaded before a service worker can
