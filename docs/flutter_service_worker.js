b3254b06b5fbf51bb7a940c7eaded776fbeca093
Choxx Delit <kiokomuinde022@gmail.com>
2025-09-21 12:19:29 +0300

    Fixed agents and testimonials screens

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,9 +3,9 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"assets/AssetManifest.bin": "5c23393fac1002d722105619be7bb902",
-"assets/AssetManifest.bin.json": "a0f17d9f9c761052ccef0de48d22169e",
-"assets/AssetManifest.json": "24f5845e89fcdfd46f6a8b8de9d0dd42",
+const RESOURCES = {"assets/AssetManifest.bin": "65026725a25ef04a3d407e480a369447",
+"assets/AssetManifest.bin.json": "6ab3b930c3b7badaf15266c756a9ac3d",
+"assets/AssetManifest.json": "36e61e1333948a8b25410910146bf5ef",
 "assets/assets/images/hero_image.webp": "8c1cc201aa44dbb7c912ae675dad555f",
 "assets/assets/images/image1.jpeg": "4b64508127f2f4a0c6037a0f2f7ff6ba",
 "assets/assets/images/image10.webp": "6f539d71b8d1dd7b7a7cc51459998baf",
@@ -196,10 +196,13 @@ const RESOURCES = {"assets/AssetManifest.bin": "5c23393fac1002d722105619be7bb902
 "assets/assets/images/image9_3.webp": "52dc36fc67ddb96f7fee383912cfaaff",
 "assets/assets/images/image9_4.webp": "ab2cab038dcda9a446d94573d4c2b739",
 "assets/assets/images/image9_5.webp": "358555c8d62f505fff0c709240dcfa69",
+"assets/assets/images/kioko_pro.png": "2ea7c8be7a43c64abe15581a6a2e2a56",
 "assets/assets/images/real_estate_hero_2.webp": "3ed638e792aebd2274f7f7c9cf6b6cc5",
 "assets/assets/images/real_estate_hero_3.webp": "b257ec719cefa6204681ea92b6d32443",
 "assets/assets/images/real_estate_hero_4.webp": "eb8eac470b36b43a16961479f950921a",
+"assets/assets/images/riki_pro.png": "cd8f1217c07fa263f9977e5200028bb4",
 "assets/assets/images/sora_logo.png": "fb00e6be2a83d149084d790eb5b2092a",
+"assets/assets/images/voke_pro.png": "b80e17b131459cb68360efea556c0c21",
 "assets/env.txt": "caa48324f8d1c3fcc5e8ab62424019f0",
 "assets/FontManifest.json": "97c2528ecc2fbf4093965257fdba1854",
 "assets/fonts/MaterialIcons-Regular.otf": "e306f7f119158a3aba04765bc6145e98",
@@ -224,14 +227,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "5c23393fac1002d722105619be7bb902
 "CNAME": "e1387523602c19a789f63aef496c64ed",
 "favicon.png": "b73dfb25a7a73361cca712dbd918df06",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "a653a966808be8fe725f443f2c45fdba",
+"flutter_bootstrap.js": "0cc903f00886bc19068b2b825cbb5d5b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "aa13e9dc757e01054c55768e2ea3892b",
 "/": "aa13e9dc757e01054c55768e2ea3892b",
-"main.dart.js": "e72b2ce6097f7ac8b327a72ed0ab26ab",
+"main.dart.js": "463251b9b1be209cf143780e20f547cf",
 "manifest.json": "e2d8aacf924b8886c558ef3a42acc6ec",
 "version.json": "b75bad869b2040f1993e54d13f3df6c6"};
 // The application shell files that are downloaded before a service worker can
