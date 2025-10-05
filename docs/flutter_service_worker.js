af2b14a1f1d1ceb3d96e5fc1ca0679052abed115
Choxx Delit <kiokomuinde022@gmail.com>
2025-10-05 17:43:16 +0300

    Perfect perfecto

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -234,7 +234,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "3c9ba839519a2aadcd8270eec0fc20b7
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "b73dfb25a7a73361cca712dbd918df06",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "931a4b596a0017c50a4ae4c61339722d",
+"flutter_bootstrap.js": "0476cfe92ed114d084d9f2ff2cc9c23b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
