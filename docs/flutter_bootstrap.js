456781d35a44c453078ac38dc15ecfd7d2e991f2
Choxx Delit <kiokomuinde022@gmail.com>
2025-10-05 17:00:58 +0300

    Fixed general issues.

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2594529629"
+    serviceWorkerVersion: "1274303004"
   }
 });
