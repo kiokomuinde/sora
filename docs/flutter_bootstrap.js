68dd384777a3881a7e863d22fa8c4b9bcf489225
Choxx Delit <kiokomuinde022@gmail.com>
2025-10-07 18:11:08 +0300

    Blog Sharing

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2606761535"
+    serviceWorkerVersion: "3953942111"
   }
 });
