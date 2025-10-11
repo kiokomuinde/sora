4da43b6e0ba75e94dc26740479dd5ff63b22d15b
Choxx Delit <kiokomuinde022@gmail.com>
2025-10-11 12:59:01 +0300

    Property Shaaring

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -236,7 +236,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "8c92133e5d43efad4ebf72b6c46563cd
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "b73dfb25a7a73361cca712dbd918df06",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "919adc98b7330f1243d8047cc16406c0",
+"flutter_bootstrap.js": "4852c1c7c8bd72e7d302a0abd6211dae",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
