03334487966f1891b5a48b1aed15c803a3404dc8
Choxx Delit <kiokomuinde022@gmail.com>
2025-10-12 15:26:49 +0300

    Dashboard Stuffing

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2244488679"
+    serviceWorkerVersion: "392210946"
   }
 });
