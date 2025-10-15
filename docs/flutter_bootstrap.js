87b98f7bdc63c57bc3e97d919844c1fef3b18397
Choxx Delit <kiokomuinde022@gmail.com>
2025-10-15 14:33:45 +0300

    Property Listings crucial updates

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "486797032"
+    serviceWorkerVersion: "3757979304"
   }
 });
