712932d1eee10db49062f928caa33e0120f21b13
Choxx Delit <kiokomuinde022@gmail.com>
2025-10-16 01:31:05 +0300

    Updated Mylistings

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3757979304"
+    serviceWorkerVersion: "3602129378"
   }
 });
